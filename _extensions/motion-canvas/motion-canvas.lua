bb62527992e8055bf09bb078f73ec2b267159780
andrie <apdevries@gmail.com>
2024-01-12 22:10:16 +0000

    feat: add extension

---
+++ _extensions/motion-canvas/motion-canvas.lua
@@ -0,0 +1,31 @@
+mc_style = [[
+<style>
+    motion-canvas-player {
+        width:  75%;
+        /* height: 25vh; */
+        display: block inline;
+        auto: true;
+    }
+</style>
+]]
+
+return {
+  ['motion-canvas'] = function(args, kwargs, meta, raw_args) 
+
+    quarto.doc.add_html_dependency({
+      name = "motion-canvas", 
+      version = "1.0.1",
+      scripts = {'motion-canvas-player.js'},
+      head = mc_style,
+    })
+
+    local src = pandoc.utils.stringify(args[1])
+    -- quarto.log.output(src)
+    -- local src = args[1]
+    local var = pandoc.utils.stringify(raw_args)
+    -- return pandoc.Str("Hello " .. var .. " from Motion-canvas!")
+    return pandoc.RawInline('html', 
+      '<motion-canvas-player src="' .. src .. '"></motion-canvas-player>'
+    )
+  end
+}
